097d3ab1b1ff26c7d009d8a44b952a4b850fe24d
mybays <mybays@qq.com>
2016-02-15 23:53:25 +0800

    asm

--- myproject/asm/example/add.s
+++ myproject/asm/example/add.s
@@ -2,9 +2,9 @@
 	.syntax unified
 sp:	.word 0x200
 reset:	.word start+1
-
-start:
-	mov r0,#4097
-	mov r1,#5
-	add r2,r1,r0
-stop:	b stop
+	.text
+start:				@ Label, not really required
+	mov r0,#4		@ Load register r0 with the value 5
+	mov r1,#5		@ Load register r1 with the value 4
+	add r2,r1,r0	@ Add r0 and r1 and store in r2
+stop:	b stop		@ Infinite loop to stop execution
